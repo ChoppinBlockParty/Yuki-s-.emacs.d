416464e047e8bf152730757e29a7a7dc60395242
Yuki <choppin.block.party@gmail.com>
2026-02-19 15:38:51 +0000

    Switch memo-config to use minibuffer via completing-read

--- configs/memo-config.el
+++ configs/memo-config.el
@@ -2,44 +2,26 @@
 ;;; Commentary:
 ;;; Code:
 
-;; Display a read-only memo buffer with rg-mode keybindings
-(defun my-memo-keybindings ()
-  "Show a read-only memo of rg-mode keybindings."
-  (interactive)
-  (my-memo--show "*memo: rg keybindings*"
-   "rg-mode Keybindings (Evil Normal State)\n"
-   "========================================\n\n"
-   "Navigation\n"
-   "----------\n"
-   "  gcd  rg-rerun-change-dir       Re-run search in a different directory\n"
-   "  gf   rg-rerun-change-files     Re-run search with different file type\n"
-   "  gr   rg-recompile              Re-run the current search\n"
-   "  gm   rg-menu                   Open the rg transient menu\n"
-   "  gl   rg-list-searches          List saved searches\n\n"
-   "Toggles\n"
-   "-------\n"
-   "  zc   rg-rerun-toggle-case      Toggle case sensitivity\n"
-   "  zi   rg-rerun-toggle-ignore    Toggle ignore file handling\n"
-   "  zl   rg-rerun-change-literal   Switch to literal search\n"
-   "  zp   rg-rerun-change-regexp    Switch to regexp search\n\n"
-   "Saving\n"
-   "------\n"
-   "  zs   rg-save-search            Save the current search\n"
-   "  zS   rg-save-search-as-name    Save search with a name\n"))
+;; rg-mode keybinding entries for the memo
+(defvar my-memo--rg-keybindings
+  '("gcd  rg-rerun-change-dir       Re-run search in a different directory"
+    "gf   rg-rerun-change-files     Re-run search with different file type"
+    "gr   rg-recompile              Re-run the current search"
+    "gm   rg-menu                   Open the rg transient menu"
+    "gl   rg-list-searches          List saved searches"
+    "zc   rg-rerun-toggle-case      Toggle case sensitivity"
+    "zi   rg-rerun-toggle-ignore    Toggle ignore file handling"
+    "zl   rg-rerun-change-literal   Switch to literal search"
+    "zp   rg-rerun-change-regexp    Switch to regexp search"
+    "zs   rg-save-search            Save the current search"
+    "zS   rg-save-search-as-name    Save search with a name")
+  "List of rg-mode keybinding entries.")
 
-;; Helper to display a read-only memo buffer with given content
-(defun my-memo--show (name &rest content)
-  "Create a read-only memo buffer called NAME with CONTENT strings.
-Uses `special-mode' so q closes the buffer."
-  (let ((buf (get-buffer-create name)))
-    (with-current-buffer buf
-      (let ((inhibit-read-only t))
-        (erase-buffer)
-        (insert (apply #'concat content))
-        (insert "\n[press q to close]"))
-      (goto-char (point-min))
-      (special-mode))
-    (pop-to-buffer buf)))
+;; Display keybindings memo in the minibuffer via completing-read
+(defun my-memo-keybindings ()
+  "Show keybinding memo in the minibuffer."
+  (interactive)
+  (completing-read "Keybindings: " my-memo--rg-keybindings nil t))
 
 (provide 'memo-config)
 ;;; memo-config.el ends here
